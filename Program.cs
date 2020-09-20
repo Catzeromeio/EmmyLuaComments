7c374aa02c6b230fd040ae5a11b0526d57bc8eee
Catzero <catzeromeio@hotmail.com>
2020-09-20 11:05:16 +0800

    Add Emmpy Proj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmmyLuaComments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
